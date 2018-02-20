44652320979aa2268763adfaba10eb56f9544aad
prashantagarwal96 <agarwal.prashant96@gmail.com>
2018-02-20 22:05:09 +0530

    Add files via upload

--- pandas2.py
+++ pandas2.py
@@ -12,4 +12,158 @@ df2
 df2.dtypes
 df.head()
 df.index
+df.describe()
+df.T
+df.sort_index(axis=1, ascending=False)
+df.sort_values(by='B')
+df['A']
+df[0:3]
+df['20130102':'20130104']
+df.loc[dates[0]]
+df.loc[:,['A','B']]
+df.loc['20130102':'20130104',['A','B']]
+df.loc['20130102',['A','B']]
+df.loc[dates[0],'A']
+df.at[dates[0],'A']
+df.iloc[3]
+df.iloc[3:5,0:2]
+df.iloc[[1,2,4],[0,2]]
+df.iloc[1:3,:]
+df.iloc[:,1:3]
+df.iloc[1,1]
+df.iat[1,1]
+df[df.A > 0]
+df[df > 0]
+df2 = df.copy()
+df2['E'] = ['one', 'one','two','three','four','three']
+df2
+s1 = pd.Series([1,2,3,4,5,6], index=pd.date_range('20130102', periods=6))
+s1
+df.at[dates[0],'A'] = 0
+df.iat[0,1] = 0
+df.loc[:,'D'] = np.array([5] * len(df))
+df
+df2 = df.copy()
+df2[df2 > 0] = -df2
+df2
+df1 = df.reindex(index=dates[0:4], columns=list(df.columns) + ['E'])
+df1.loc[dates[0]:dates[1],'E'] = 1
+df1
+df1.dropna(how='any')
+df1.fillna(value=5)
+pd.isna(df1)
+df.mean()
+df.mean(1)
+s = pd.Series([1,3,5,np.nan,6,8], index=dates).shift(2)
+s
+df.sub(s, axis='index')
+df.apply(np.cumsum)
+df.apply(lambda x: x.max() - x.min())
+s = pd.Series(np.random.randint(0, 7, size=10))
+s
+s.value_counts()
+s = pd.Series(['A', 'B', 'C', 'Aaba', 'Baca', np.nan, 'CABA', 'dog', 'cat'])
+s.str.lower()
+df = pd.DataFrame(np.random.randn(10, 4))
+df
+pieces = [df[:3], df[3:7], df[7:]]
+pd.concat(pieces)
+left = pd.DataFrame({'key': ['foo', 'foo'], 'lval': [1, 2]})
+right = pd.DataFrame({'key': ['foo', 'foo'], 'rval': [4, 5]})
+left
+right
+pd.merge(left, right, on='key')
+df = pd.DataFrame(np.random.randn(8, 4), columns=['A','B','C','D'])
+df
+ df = pd.DataFrame({'A' : ['foo', 'bar', 'foo', 'bar',
+                            'foo', 'bar', 'foo', 'foo'],
+                     'B' : ['one', 'one', 'two', 'three',
+                                   'two', 'two', 'one', 'three'],
+                      'C' : np.random.randn(8),
+                      'D' : np.random.randn(8)})
+df
+df.groupby('A').sum()
+df.groupby(['A','B']).sum()
+tuples = list(zip(*[['bar', 'bar', 'baz', 'baz',
+                         'foo', 'foo', 'qux', 'qux'],
+                        ['one', 'two', 'one', 'two',
+                         'one', 'two', 'one', 'two']]))
+
+stacked = df2.stack()
+stacked
+stacked.unstack()
+stacked.unstack(1)
+stacked.unstack(0)
+df = pd.DataFrame({'A' : ['one', 'one', 'two', 'three'] * 3,
+                       'B' : ['A', 'B', 'C'] * 4,
+                       'C' : ['foo', 'foo', 'foo', 'bar', 'bar', 'bar'] * 2,
+                       'D' : np.random.randn(12),
+                       'E' : np.random.randn(12)})
+df
+
+pd.pivot_table(df, values='D', index=['A', 'B'], columns=['C'])
+rng = pd.date_range('1/1/2012', periods=100, freq='S')
+ts = pd.Series(np.random.randint(0, 500, len(rng)), index=rng)
+ts.resample('5Min').sum()
+rng = pd.date_range('3/6/2012 00:00', periods=5, freq='D')
+ts = pd.Series(np.random.randn(len(rng)), rng)
+ts
+ts_utc.tz_convert('US/Eastern')
+rng = pd.date_range('1/1/2012', periods=5, freq='M')
+ts = pd.Series(np.random.randn(len(rng)), index=rng)
+ts
+prng = pd.period_range('1990Q1', '2000Q4', freq='Q-NOV')
+ts = pd.Series(np.random.randn(len(prng)), prng)
+ts.index = (prng.asfreq('M', 'e') + 1).asfreq('H', 's') + 9
+ts.head()
+df = pd.DataFrame({"id":[1,2,3,4,5,6], "raw_grade":['a', 'b', 'b', 'a', 'a', 'e']})
+df["grade"] = df["raw_grade"].astype("category")
+df["grade"]
+df["grade"].cat.categories = ["very good", "good", "very bad"]
+df["grade"] = df["grade"].cat.set_categories(["very bad", "bad", "medium", "good", "very good"])
+df["grade"]
+df.sort_values(by="grade")
+df.groupby("grade").size()
+ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+ts = ts.cumsum()
+ts.plot()
+df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index,columns=['A', 'B', 'C', 'D']) 
+df = df.cumsum()
+plt.figure(); df.plot(); plt.legend(loc='best')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
